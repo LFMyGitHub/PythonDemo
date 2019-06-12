957fc3c24a8e14084eb5fa00e3de88b3b5b9e6e8
545945521 <Debug_longfeng>
2019-06-12 09:57:01 +0800

    helloword

---
+++ README.md
@@ -0,0 +1,6 @@
+python学习项目  
+
+检查python版本  
+python --version  
+安装python3  
+sudo apt-get install python3
---
+++ cli/helloworld.py
@@ -0,0 +1,13 @@
+#打印字符串
+print('hello world')
+#多个字符串用逗号隔开
+print('This is pythone3 Demo','hello','jack')
+#打印整数
+print(300)
+#打印计算结果
+print(100+200)
+#输入
+name = input()
+print(name)
+print('1024*768=',1024*768)
+input('\n\n按下enter退出')
