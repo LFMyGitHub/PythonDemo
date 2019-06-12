6f8a9133c7b46a198ac92b7c38ffa7162369ba14
545945521 <Debug_longfeng>
2019-06-12 10:19:16 +0800

    数据类型和变量

--- cli/helloworld.py
+++ cli/helloworld.py
@@ -10,4 +10,29 @@ print(100+200)
 name = input()
 print(name)
 print('1024*768=',1024*768)
+
+#python数据类型:整数，浮点数，字符串，布尔值(True/False)，空值(None不能理解为0)，列表，字典，自定义等数据类型
+#转义字符
+print('I\'m "OK"')
+print('\n','\\n表示换行')
+print('r\'\'',r'内部不转意\\')
+print('''第一行
+第二行
+第三行''')
+print(r'''第一行,\n
+第二行''')
+#动态语言:变量类型不固定
+a = 123
+print('a=',a)
+a = 'adc'
+print('a=',a)
+#静态语言:Java
+#
+
+
+
+
+
+
+
 input('\n\n按下enter退出')
