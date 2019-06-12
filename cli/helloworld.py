a8bdeeef38c8117ef33e5cae117f4d04019da516
545945521 <Debug_longfeng>
2019-06-12 10:58:13 +0800

    字符串和编码

--- cli/helloworld.py
+++ cli/helloworld.py
@@ -27,12 +27,38 @@ print('a=',a)
 a = 'adc'
 print('a=',a)
 #静态语言:Java
-#
 
+#字符串和编码
+#ASCII编码:数字大小写英文共计127个字符(1字节)
+#GB2312编码:中文编码
+#各国标准不一，多语言混合导致乱码
+#Unicode编码统一语言规范(通常2字节,非常偏僻的字符4字节)
+#全英文文本Unicode比ASCII多需要一倍存储空间
+#可变长编码UTF-8(英文1字节，汉字3字节，很生僻的字符4-6字节，节省空间)
+print(ord('A'))
+print(ord('中'))
+print(chr(66))
+print(chr(555))
+print('\u4e2d\u6587')
+#网络传输或保存到磁盘需把str转为bytes
+x = b'ABC'
+print(x)
+#Unicode表示的str可以通过encode()指定编码byte
+print('ABX'.encode('ascii'))
+print('中午'.encode('utf-8'))
+#decode()转bytes为str
+print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'))
+#小部分无效的字节，可以传入errors='ignore'忽略错误的字节
+print(b'\xe4\xb8\xad\xff'.decode('utf-8',errors='ignore'))
+#计算str包含多少字符或者bytes字节数
+print(len('ABC'))
+print(len(b'ABC'))
 
+#!/usr/bin/env python3 告诉Linux/OS X系统，这是一个Python可执行程序，Windows系统会忽略这个注释
+# -*- coding: utf-8 -*- 告诉Python解释器，按照UTF-8编码读取源代码，否则，你在源代码中写的中文输出可能会有乱码
 
-
-
-
+#格式化字符串(%d整数,%f浮点数,%s字符串,%x十六进制整数)
+print('%2d-%02d'%(2,1))
+print('%.2f'%3.14156)
 
 input('\n\n按下enter退出')
